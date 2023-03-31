f9556ef50469d520352dd4b3a31c2bd5e0b2955d
MarkRam2 <116022322+MarkRam2@users.noreply.github.com>
2023-03-31 11:30:09 -0700

    fixed hash password error

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -17,34 +17,6 @@ namespace cookEaseBackEnd.Controllers
         public UserController(UserService dataFromService){
             _data = dataFromService;
         }
-        // login end point
-
-        // add a user 
-            // if the user alredy exists
-            // if they don't exist create the account
-            // else throw a false
-            public bool AddUser(CreateAccountDto UserToAdd){
-                return _data.AddUser(UserToAdd);
-            }
-        // update USER ACCOUNT
-        // delete user account
-        public PasswordDto HashPassword(string password){
-            PasswordDto newHashedPassword = new PasswordDto();
-            byte[] SaltByte = new byte[64];
-            var provider = new RNGCryptoServiceProvider();
-            provider.GetNonZeroBytes(SaltByte);
-
-            var Salt = Convert.ToBase64String(SaltByte);
-
-            Rfc2898DeriveBytes rfc2898DeriveBytes = new Rfc2898DeriveBytes(password, SaltByte, 10000);
-
-            var Hash = Convert.ToBase64String(rfc2898DeriveBytes.GetBytes(256));
-
-            newHashedPassword.Salt = Salt;
-            newHashedPassword.Hash = Hash;
-
-            return newHashedPassword;
-        }
 
         public bool VerifyUserPassword(string? Password, string? storedHash, string? storedSalt){
             var SaltBytes = Convert.FromBase64String(storedSalt);
--- Services/UserService.cs
+++ Services/UserService.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using cookEaseBackEnd.Models;
 using cookEaseBackEnd.Models.Dto;
 using cookEaseBackEnd.Services.Context;
+using System.Security.Cryptography;
 
 namespace cookEaseBackEnd.Services
 {
@@ -36,6 +37,26 @@ namespace cookEaseBackEnd.Services
             }
             return result;
         }
+        public PasswordDto HashPassword(string? password){
+            PasswordDto newHashedPassword = new PasswordDto();
+            byte[] SaltByte = new byte[64];
+            var provider = new RNGCryptoServiceProvider();
+            provider.GetNonZeroBytes(SaltByte);
+
+            var Salt = Convert.ToBase64String(SaltByte);
+
+            Rfc2898DeriveBytes rfc2898DeriveBytes = new Rfc2898DeriveBytes(password, SaltByte, 10000);
+
+            var Hash = Convert.ToBase64String(rfc2898DeriveBytes.GetBytes(256));
+
+            newHashedPassword.Salt = Salt;
+            newHashedPassword.Hash = Hash;
+
+            return newHashedPassword;
+        }
+        // public bool VerifyUserPassword(string? Password, string? storedHash, string? storedSalt){
+        //     var SaltBytes = Convert.
+        // }
 
     }
 }
