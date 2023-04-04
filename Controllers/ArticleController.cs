6be4eaa9a48cb9cd715183703321de00125541fd
MarkRam2 <116022322+MarkRam2@users.noreply.github.com>
2023-04-04 15:47:16 -0700

    article Controller and service

--- Controllers/ArticleController.cs
+++ Controllers/ArticleController.cs
@@ -2,14 +2,74 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using cookEaseBackEnd.Models;
+using cookEaseBackEnd.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace cookEaseBackEnd.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class ArticleModel
+    public class ArticleController : ControllerBase
     {
+        private readonly ArticleService _data;
+        public ArticleController(ArticleService dataFromService){
+            _data = dataFromService;
+        }
+
+        [HttpPost]
+        [Route("AddArticleItem")]
+        public bool AddArtcleItem( ArticleItemModel newArticelItem){
+            return _data.AddArtcleItem(newArticelItem);
+        }
+
+        [HttpGet]
+        [Route("GetAllArticleItems")]
+        public IEnumerable<ArticleItemModel> GetAllArticleItems(){
+            return _data.GetAllArticleItems();
+        }
+
+        [HttpGet]
+        [Route("GetItemsByUserId/{UserId}")]
+        public IEnumerable<ArticleItemModel> GetItemsByUserId(int UserID){
+            return _data.GetItemsByUserId(UserID);
+        }
+
+        [HttpGet]
+        [Route("GetItemsByCategory/{Categories}")]
+        public IEnumerable<ArticleItemModel> GetItemsByCategory(string Categories){
+            return _data.GetItemsByCategory(Categories);
+        }
+
+        [HttpGet]
+        [Route("GetItemsByDate/{Date}")]
+        public IEnumerable<ArticleItemModel> GetItemsByDate(string Date){
+            return _data.GetItemsByDate(Date);
+        }
         
+        [HttpGet]
+        [Route("GetItemsByTitle/{Title}")]
+        public IEnumerable<ArticleItemModel> GetItemsByTitle(string Title){
+            return _data.GetItemsByTitle(Title);
+        }
+
+        [HttpGet]
+        [Route("GetItemsByTags/{Tags}")]
+        public List<ArticleItemModel> GetItemsByTags(string Tags){
+            return _data.GetItemsByTags(Tags);
+        }
+
+        [HttpGet]
+        [Route("GetItemsByPublisherName/{PublisherName}")]
+        public IEnumerable<ArticleItemModel> GetItemsByPublisherName(string PublisherName){
+            return _data.GetItemsByPublisherName(PublisherName);
+        }
+
+        [HttpGet]
+        [Route("GetItemsByPublished")]
+        public IEnumerable<ArticleItemModel> GetItemsByPublished(){
+            return _data.GetItemsByPublished();
+        }
+
     }
 }
--- Services/ArticleService.cs
+++ Services/ArticleService.cs
@@ -2,11 +2,67 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using cookEaseBackEnd.Models;
+using cookEaseBackEnd.Services.Context;
 
 namespace cookEaseBackEnd.Services
 {
     public class ArticleService
     {
-        
+        private readonly DataContext _context;
+        public ArticleService(DataContext context){
+            _context = context;
+        }
+
+        public bool AddArtcleItem( ArticleItemModel newArticelItem){
+            _context.Add(newArticelItem);
+            return _context.SaveChanges() != 0;
+        }
+
+        public IEnumerable<ArticleItemModel> GetAllArticleItems(){
+            return _context.ArticleInfo;
+        }
+
+        public IEnumerable<ArticleItemModel> GetItemsByUserId(int UserId){
+            return _context.ArticleInfo.Where(item => item.UserID == UserId);
+        }
+
+        public IEnumerable<ArticleItemModel> GetItemsByCategory(string Categories){
+            return _context.ArticleInfo.Where(item => item.Categories == Categories);
+        }
+
+        public IEnumerable<ArticleItemModel> GetItemsByDate(string Date){
+            return _context.ArticleInfo.Where(item => item.Date == Date);
+        }
+
+        public IEnumerable<ArticleItemModel> GetItemsByTitle(string Title){
+            return _context.ArticleInfo.Where(item => item.Title == Title);
+        }
+
+        public List<ArticleItemModel> GetItemsByTags(string Tags){
+
+            List<ArticleItemModel> AllArticlesWithTags = new List<ArticleItemModel>();
+            var allItems = GetAllArticleItems().ToList();
+
+            for(int i=0; i< allItems.Count; i++){
+                ArticleItemModel Item = allItems[i];
+                var itemArr = Item.Tags.Split(",");
+                for(int j=0; j < itemArr.Length; j++){
+                    // checking if the item array has the tag we're looking for
+                    if(itemArr[j].Contains(Tags)){
+                        AllArticlesWithTags.Add(Item);
+                    }
+                }
+            }
+            return AllArticlesWithTags;
+        }
+
+        public IEnumerable<ArticleItemModel> GetItemsByPublisherName(string PublisherName){
+            return _context.ArticleInfo.Where(item => item.PublisherName == PublisherName);
+        }
+
+        public IEnumerable<ArticleItemModel> GetItemsByPublished(){
+            return _context.ArticleInfo.Where(item => item.isPublished);
+        }
     }
 }
