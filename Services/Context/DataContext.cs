d2638c12c4c3124af06d909c89caf34a9f9c91c2
MarkRam2 <116022322+MarkRam2@users.noreply.github.com>
2023-04-27 15:12:56 -0700

    yes

--- Services/Context/DataContext.cs
+++ Services/Context/DataContext.cs
@@ -12,7 +12,6 @@ namespace cookEaseBackEnd.Services.Context
         public DbSet<UserModel> UserInfo { get; set; }
         public DbSet<RecipeItemModel> RecipeInfo { get; set; }
         public DbSet<ArticleItemModel> ArticleInfo { get; set; }
-
         public DbSet<IngredientsItemModel> IngredientInfo { get; set; }
         public DbSet<NutritionItemModel> NutritionInfo { get; set; }
 
