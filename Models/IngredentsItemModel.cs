12556e7e1a338460bc545a28f6d8c9ce87315569
MarkRam2 <116022322+MarkRam2@users.noreply.github.com>
2023-05-01 14:52:42 -0700

    making progress

--- Models/IngredentsItemModel.cs
+++ Models/IngredentsItemModel.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,13 +10,29 @@ namespace cookEaseBackEnd.Models
 {
 
     [Keyless]
-    public class IngredentsItemModel
+    public class IngredientsItemModel
     {
         public int Id { get; set; }
         public int? RecipeId { get; set; }
         public string? Ingredent { get; set; }
         public int? Weight { get; set; }
 
-        public IngredentsItemModel() { }
+        public IngredientsItemModel() { }
+    }
+
+    public class IngredentArray
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            IngredientsItemModel IngArray = new IngredientsItemModel
+            {
+                
+            };
+            var options = new JsonSerializerOptions();
+            
+            string json = JsonSerializer.Serialize<IngredientsItemModel>(IngArray);
+        }
     }
 }
